9317cfea57bd65771f35b7a5ce7863d9b66a1478
User Rdeco <rdeco@www3712.sakura.ne.jp>
2018-02-12 16:43:21 +0900

    name_enbedded

--- src/stprj/production_settings.py
+++ src/stprj/production_settings.py
@@ -41,7 +41,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'template'),
+            os.path.join(BASE_DIR, 'app/template'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
